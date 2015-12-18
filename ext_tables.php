fa94ac2856f80940d0e38890b352f96a6ecee529
Dirk Wenzel <wenzel@cps-it.de>
2015-12-18 13:37:28 +0100

    [TASK] model ExportTarget implemented.

--- ext_tables.php
+++ ext_tables.php
@@ -9,11 +9,11 @@ if (TYPO3_MODE === 'BE') {
 		'',
 		'',
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/BackendModule/',
-		array(
+		[
 			'access' => 'user,group',
 			'icon' => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
 			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod.xlf',
-		)
+		]
 	);
 
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
@@ -21,13 +21,14 @@ if (TYPO3_MODE === 'BE') {
 		'system',
 		'Import',
 		'',
-		array(
+		[
 			'Import' => 'index,importTask,importSet',
-		),
-		array(
+		],
+		[
 			'access' => 'user,group',
 			'icon' => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
 			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_import.xlf',
-		)
+		]
 	);
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_t3import_domain_model_exporttarget');
 }
