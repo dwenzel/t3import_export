9ea8bcd4f00fc471105623e22655ca29ac158a76
Vladimir Falcon <falcon@cps-it.de>
2016-04-27 14:53:52 +0200

    [TASK] Added set configuration settings to DataTargetFactory

--- Classes/Persistence/Factory/DataTargetFactory.php
+++ Classes/Persistence/Factory/DataTargetFactory.php
@@ -1,6 +1,7 @@
 <?php
 namespace CPSIT\T3import\Persistence\Factory;
 
+use CPSIT\T3import\ConfigurableInterface;
 use CPSIT\T3import\Factory\AbstractFactory;
 use CPSIT\T3import\Persistence\DataTargetInterface;
 use CPSIT\T3import\Persistence\DataTargetRepository;
@@ -32,64 +33,66 @@ use CPSIT\T3import\InvalidConfigurationException;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
-class DataTargetFactory extends AbstractFactory {
-	const DEFAULT_DATA_TARGET_CLASS = DataTargetRepository::class;
+class DataTargetFactory extends AbstractFactory
+{
+    const DEFAULT_DATA_TARGET_CLASS = DataTargetRepository::class;
 
-	/**
-	 * Builds a factory object
+    /**
+     * Builds a factory object
+     *
+     * @param array $settings
+     * @param string $identifier
+     * @return DataTargetInterface
+     * @throws \CPSIT\T3import\InvalidConfigurationException
+     * @throws \CPSIT\T3import\MissingClassException
+     * @throws MissingInterfaceException
+     */
+    public function get(array $settings, $identifier = null)
+    {
+        $dataTargetClass = self::DEFAULT_DATA_TARGET_CLASS;
+        if (isset($settings['class'])) {
+            $dataTargetClass = $settings['class'];
+        }
+        if (!class_exists($dataTargetClass)) {
+            throw new MissingClassException(
+                'Missing target.class ' . $dataTargetClass .
+                ' in configuration for import task ' . $identifier,
+                1451043513
+            );
+        }
+        if (!in_array(DataTargetInterface::class, class_implements($dataTargetClass))) {
+            throw new MissingInterfaceException(
+                'Missing interface in configuration for task ' . $identifier . ' Class ' . $dataTargetClass .
+                ' does not implement required interface ' . DataTargetInterface::class . '.',
+                1451045997
+            );
+        }
+        if (!isset($settings['object']['class'])) {
+            throw new InvalidConfigurationException(
+                'Invalid configuration for import task ' . $identifier .
+                ': target.object.class not set.',
+                1451043340
+            );
+        }
+        $objectClass = $settings['object']['class'];
+        if (!class_exists($objectClass)) {
+            throw new MissingClassException(
+                'Missing class ' . $objectClass .
+                ' in configuration for task ' . $identifier,
+                1451043367
+            );
+        }
+        /** @var DataTargetInterface $target */
+        $target = $this->objectManager->get(
+            $dataTargetClass,
+            $objectClass
+        );
 
+        if ($target instanceof ConfigurableInterface && isset($settings['config'])) {
+            $target->setConfiguration($settings['config']);
+        }
 
-
-
-*
-*@param array $settings
-	 * @param string $identifier
-	 * @return DataTargetInterface
-	 * @throws \CPSIT\T3import\InvalidConfigurationException
-	 * @throws \CPSIT\T3import\MissingClassException
-	 * @throws MissingInterfaceException
-	 */
-	public function get(array $settings, $identifier = null) {
-		$dataTargetClass = self::DEFAULT_DATA_TARGET_CLASS;
-		if (isset($settings['class'])) {
-			$dataTargetClass = $settings['class'];
-		}
-		if (!class_exists($dataTargetClass)) {
-			throw new MissingClassException(
-				'Missing target.class ' . $dataTargetClass .
-				' in configuration for import task ' . $identifier,
-				1451043513
-			);
-		}
-		if (!in_array(DataTargetInterface::class, class_implements($dataTargetClass))) {
-			throw new MissingInterfaceException(
-				'Missing interface in configuration for task '. $identifier . ' Class ' . $dataTargetClass .
-				' does not implement required interface ' . DataTargetInterface::class . '.',
-				1451045997
-			);
-		}
-		if (!isset($settings['object']['class'])) {
-			throw new InvalidConfigurationException(
-				'Invalid configuration for import task '. $identifier .
-				': target.object.class not set.',
-				1451043340
-			);
-		}
-		$objectClass = $settings['object']['class'];
-		if (!class_exists($objectClass)) {
-			throw new MissingClassException(
-				'Missing class ' . $objectClass .
-				' in configuration for task ' . $identifier,
-				1451043367
-			);
-		}
-		/** @var DataTargetInterface $target */
-		$target = $this->objectManager->get(
-			$dataTargetClass,
-			$objectClass
-		);
-
-		return $target;
-	}
+        return $target;
+    }
 
 }
