8ec442e5614a0a556ddf1e75b8cc8dcf2a49e1b2
Dirk Wenzel <wenzel@cps-it.de>
2016-08-13 16:21:57 +0200

    [TASK] unit tests for DatabaseConnectionService added. Code cleanup.

--- Classes/Service/DatabaseConnectionService.php
+++ Classes/Service/DatabaseConnectionService.php
@@ -23,58 +23,86 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  *  GNU General Public License for more details.
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
-class DatabaseConnectionService implements SingletonInterface {
 
-	/**
-	 * @var array<DatabaseConnection>
-	 */
-	protected static $dataBases = [];
+/**
+ * Class DatabaseConnectionService
+ * Registers database connections and makes them
+ * available by identifier
+ *
+ * @package CPSIT\T3importExport\Service
+ */
+class DatabaseConnectionService implements SingletonInterface
+{
 
-	/**
-	 * Registers a database instance
-	 *
-	 * @param string $identifier
-	 * @param string $hostName
-	 * @param string $databaseName
-	 * @param string $userName
-	 * @param string $password
-	 * @param int $port
-	 */
-	public static function register($identifier, $hostName = '127.0.0.1', $databaseName, $userName, $password, $port = 3306) {
-		if (!self::isRegistered($identifier)) {
-			/** @var DatabaseConnection $database */
-			$database = GeneralUtility::makeInstance(DatabaseConnection::class);
+    /**
+     * All registered databases
+     *
+     * @var array<DatabaseConnection>
+     */
+    protected static $dataBases = [];
 
-			$database->setDatabaseHost($hostName);
-			$database->setDatabaseName($databaseName);
-			$database->setDatabaseUsername($userName);
-			$database->setDatabasePassword($password);
-			$database->setDatabasePort($port);
-			self::$dataBases[$identifier] = $database;
-		}
-	}
+    /**
+     * Registers a database instance
+     * Will silently fail if another database
+     * has been registered for this identifier already
+     *
+     * @param string $identifier
+     * @param string $hostName
+     * @param string $databaseName
+     * @param string $userName
+     * @param string $password
+     * @param int $port
+     */
+    public static function register(
+        $identifier,
+        $hostName = '127.0.0.1',
+        $databaseName,
+        $userName,
+        $password,
+        $port = 3306
+    ) {
+        if (!self::isRegistered($identifier)) {
+            /** @var DatabaseConnection $database */
+            $database = GeneralUtility::makeInstance(DatabaseConnection::class);
 
-	/**
-	 * @param $identifier
-	 * @return DatabaseConnection
-	 * @throws MissingDatabaseException
-	 */
-	public function getDatabase($identifier) {
-		if (self::isRegistered($identifier)) {
-			return self::$dataBases[$identifier];
-		}
-		throw new MissingDatabaseException(
-			'No database registered for identifier ' . $identifier,
-			1449363030
-		);
-	}
+            $database->setDatabaseHost($hostName);
+            $database->setDatabaseName($databaseName);
+            $database->setDatabaseUsername($userName);
+            $database->setDatabasePassword($password);
+            $database->setDatabasePort($port);
+            self::$dataBases[$identifier] = $database;
+        }
+    }
 
-	/**
-	 * @param $identifier
-	 * @return bool
-	 */
-	public static function isRegistered($identifier) {
-		return isset(self::$dataBases[$identifier]);
-	}
+    /**
+     * Gets a registered database instance by
+     * its identifier t
+     *
+     * @param string $identifier Identifier for the requested database
+     * @return DatabaseConnection
+     * @throws MissingDatabaseException Thrown if the requested database does not exist
+     */
+    public function getDatabase($identifier)
+    {
+        if (self::isRegistered($identifier)) {
+            return self::$dataBases[$identifier];
+        }
+        throw new MissingDatabaseException(
+            'No database registered for identifier ' . $identifier,
+            1449363030
+        );
+    }
+
+    /**
+     * Tells if a database has been registered for
+     * the given identifier
+     *
+     * @param string $identifier
+     * @return bool
+     */
+    public static function isRegistered($identifier)
+    {
+        return isset(self::$dataBases[$identifier]);
+    }
 
 }
