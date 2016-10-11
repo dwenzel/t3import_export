37a69a251e3ae3ea1df197e01bf84c8ea0d90a3e
Dirk Wenzel <wenzel@cps-it.de>
2016-10-11 10:51:00 +0200

    [BUGFIX] deprecated configuration for backend module removed. Missing labels added.

--- Configuration/BackendModule/conf.php
+++
@@ -1,5 +0,0 @@
-<?php
-$MLANG['default']['tabs_images']['tab'] = 'new.gif';
-$MLANG['default']['ll_ref'] = 'LLL:EXT:t3import_export/Resources/Private/Language/locallang_mod.xlf';
-$MCONF['defaultMod'] = 'new';
-$MCONF['name'] = 'user';
