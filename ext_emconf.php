0a29320cc85332ffb68e3563dd114b2c32428f8f
Dirk Wenzel <wenzel@cps-it.de>
2017-06-27 17:13:22 +0200

    [TASK] require TYPO3   6.2.0-8.99.99 in ext_emconf.php too

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array(
   array(
     'depends' =>
     array(
-      'typo3' => '6.2.0-7.99.99',
+      'typo3' => '6.2.0-8.99.99',
       'php' => '5.4.0-0.0.0',
     ),
     'conflicts' =>
