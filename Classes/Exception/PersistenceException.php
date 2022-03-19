8a227246a6813aa8e3a31012113a3650275301a7
Vladimir Falcon Piva <vladimirfalcon@gmail.com>
2022-03-19 16:26:13 +0100

    Feature Persistence Exception class

---
+++ Classes/Exception/PersistenceException.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of the t3import_export project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ */
+
+namespace CPSIT\T3importExport\Exception;
+
+class PersistenceException extends \Exception
+{
+
+}
