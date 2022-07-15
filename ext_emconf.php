b525c6f0bda639f9223434c9b985cf7ed9727b57
Vladimir Falcon <v.falcon@familie-redlich.de>
2022-07-15 14:22:05 +0200

    [RELEASE] Release of t3import_export 1.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 1,
-	'version' => '1.0.2',
+	'version' => '1.0.3',
 	'constraints' =>
 	array (
 		'depends' =>
