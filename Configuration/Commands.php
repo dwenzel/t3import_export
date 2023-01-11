93624d3bc2f9750662fd605594643cd8a2a50d6a
Vladimir Falcon <v.falcon@familie-redlich.de>
2023-01-11 13:29:05 +0100

    Fixed command registration

--- Configuration/Commands.php
+++ Configuration/Commands.php
@@ -1,7 +1,8 @@
 <?php
 declare(strict_types=1);
+if ((TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Core\Information\Typo3Version::class))->getMajorVersion() < 10) {
+    /** @var \CPSIT\T3importExport\Configuration\Extension $configuration */
+    $configuration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\CPSIT\T3importExport\Configuration\Extension::class);
+    return $configuration->getCommandsToRegister();
+}
 
-/** @var \CPSIT\T3importExport\Configuration\Extension $configuration */
-$configuration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\CPSIT\T3importExport\Configuration\Extension::class);
-
-return $configuration->getCommandsToRegister();
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,11 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-# register extbase command controllers for import and export
-\CPSIT\T3importExport\Configuration\Extension::registerLegacyCommands();
+
+if ((TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Core\Information\Typo3Version::class))->getMajorVersion() < 10) {
+    # register extbase command controllers for import and export
+    \CPSIT\T3importExport\Configuration\Extension::registerLegacyCommands();
+}
 
 // register custom implementation of PersistentObjectConverter
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\CPSIT\T3importExport\Property\TypeConverter\PersistentObjectConverter::class);
