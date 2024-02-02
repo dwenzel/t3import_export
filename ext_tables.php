208d7db09618e4a8579431b2538f39a8a34579ef
Marno Schmigotzki <m.schmigotzki@familie-redlich.de>
2024-02-02 17:16:06 +0100

    [TASK] removed unnecessary condition

--- ext_tables.php
+++ ext_tables.php
@@ -5,6 +5,4 @@ if (!defined('TYPO3')) {
 
 \CPSIT\T3importExport\Configuration\Extension::registerAndConfigureModules();
 
-if (TYPO3_MODE === 'BE') {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_t3importexport_domain_model_exporttarget');
-}
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_t3importexport_domain_model_exporttarget');
